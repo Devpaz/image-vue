47716c6924644c928d09e2ba77062a955e66c0bc
Paolo Paz <sl.paolo.paz@gmail.com>
2023-03-03 15:39:02 -0500

    select your image

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeImage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeImage from '@/components/HomeImage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeImage
   }
 }
 </script>
---
+++ src/components/HomeImage.vue
@@ -0,0 +1,81 @@
+<template>
+    <h1>Select a City</h1>
+    <div class="container">
+        <div @click="handleClickRight">
+            <img class="image-right" 
+            :src ="imageRight">
+            
+            <h4>Arequipa</h4>
+        </div>
+        <div @click="handleClickCenter">
+            <img class="image-center" 
+            :src ="imageCenter">
+            <h4>Ica</h4>
+        </div>
+        <div @click="handleClickLeft">
+            <img class="image-left" 
+            :src="imageLeft">
+            <h4>Lima</h4>
+        </div>
+    </div>
+    <h3>{{city}}</h3>
+
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            city: 'Tacna',
+            imageRight:require('@/assets/city/AQP.jpg'),
+            imageCenter:require('@/assets/city/ICA.jpg'),
+            imageLeft:require('@/assets/city/LIM.jpg')
+
+        }
+    },
+    methods:{
+        handleClickRight() {
+            this.imageRightOld=this.imageRight
+            this.imageRight=this.imageCenter
+            this.imageCenter=this.imageRightOld
+            this.city = 'Arequipa'
+
+        },
+        handleClickCenter() {
+            this.city = 'Ica'
+        },
+        handleClickLeft() {
+            this.city = 'Lima'
+            this.imageCenterOld=this.imageCenter
+            this.imageCenter=this.imageLeft
+            this.imageLeft=this.imageCenterOld
+        }
+    }
+
+}
+</script>
+
+<style>
+.container {
+    display: flex;
+    justify-content: center;
+
+}
+.image-right {
+    margin: 0 10px;
+    width: 400px;
+    height: 400px;
+    object-fit: none;
+}
+.image-center{
+    margin: 0 8px;
+    width: 700px;
+    height: 400px;
+}
+.image-left {
+    margin: 0 10px;
+    width: 400px;
+    height: 400px;
+    object-fit: none;
+}
+</style>
